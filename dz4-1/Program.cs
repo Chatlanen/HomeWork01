dee8a0adcd63fe882f8ebf918e8b096c0ff0b526
Miftakhov.AI <Miftakhov.AI@fnkc-fmba.net>
2023-12-04 12:59:08 +0300

    Inital commit

---
+++ dz4-1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int [] arr = new int[10];
+int n_count = 0;
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length;i++){
+    arr[i] = rnd.Next(1,100);
+    if (arr[i] >=20 && arr[i] <= 90){
+        n_count++;
+    } 
+}
+Console.WriteLine(string.Join(",", arr));
+Console.WriteLine($"Чисел от 20 до 90: {n_count}");
---
+++ dz4-3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью.
+// Найдите разницу между максимальным и минимальным элементов массива.
+Random rnd = new Random();
+Double [] arr = new Double[10];
+
+for (int i = 0; i < arr.Length;i++){
+    arr[i] = rnd.NextDouble() * 100 - 20;
+    Console.WriteLine($"{arr[i]}");
+}
+
+double min = arr[0];
+double max = arr[0];
+
+for (int i = 0; i < arr.Length;i++){
+    min = (arr[i] < min) ? arr[i] : min;
+    max = (arr[i] > max) ? arr[i] : max;
+}
+
+Console.WriteLine($"Минимальное элемент массива: {min}");
+Console.WriteLine($"Максимальное элемент массива: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным: {max - min}");
